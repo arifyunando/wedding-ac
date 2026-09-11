07ea337e469f296b0566dca17ece9b8c9a01e01b
arifyunando <arifyunando@gmail.com>
2026-09-11 21:10:12 +0200

    🔧 reconfigure prisma generate client output

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "../prisma/generated/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from "$env/static/private";
 
